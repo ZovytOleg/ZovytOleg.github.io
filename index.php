9f927096784d025c7cad0d3929797192e60b88c7
ZovytOleg <48530996+ZovytOleg@users.noreply.github.com>
2021-09-20 21:40:26 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 # Принимаем запрос
 $data = json_decode(file_get_contents('php://input'), TRUE);
-//file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
 
 
 //https://api.telegram.org/bot*Токен бота*/setwebhook?url=*ссылка на бота*
